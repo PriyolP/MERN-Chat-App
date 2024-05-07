fb5188cbbb2bf484305de9316bd86eaac218fff6
PriyolP <preyool92@gmail.com>
2024-05-07 03:02:33 -0300

    added mongoose succesfully

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,64 @@
 import React from "react";
+import {
+  Box,
+  Text,
+  Container,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work sans"
+          textAlign="center"
+          color="black"
+        >
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+        color="black"
+      >
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
